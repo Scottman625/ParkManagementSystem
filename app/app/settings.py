7ebcf26f50d45b7dcee381418ec58b5951e657bb
Scottman625 <scottman608@gmail.com>
2025-03-20 21:12:52 +0800

    update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
