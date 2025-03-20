1fdd731380be56f879fb19b7e1a62037691f4215
Scottman625 <scottman608@gmail.com>
2025-03-20 18:55:21 +0800

    update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -174,7 +174,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'drf_yasg.schema.SchemaGenerator',
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
